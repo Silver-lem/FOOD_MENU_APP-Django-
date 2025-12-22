5af3db6438304867d890b4890711d8f7ec2e5fe7
Geethika <april.appy24@gmail.com>
2025-12-23 04:00:28 +0530

    context passed to templates

--- display/views.py
+++ display/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item
+from django.template import loader
 
 # Create your views here.
 def index(request):
     item_list = Item.objects.all()
-    return HttpResponse(item_list)
+    template = loader.get_template('display/index.html')
+    context = {
+        'item_list' : item_list,
+    }
+    #Data obtained from the database - django takes the templates and combines it with the data obtained from db then it renders an output,currently context is empty
+    return HttpResponse(template.render(context,request))
 
 def item(request):
     return HttpResponse('This is an item view')
