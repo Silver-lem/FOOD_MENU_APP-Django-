2d7b0a2ee074d828083cdddc75c059b429bbc60f
Geethika <april.appy24@gmail.com>
2025-12-24 02:30:04 +0530

    Namespacing

--- display/urls.py
+++ display/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'display'
 urlpatterns = [
+    #/display/
     path('',views.index,name='index'),
     #/display/id
     path('<int:item_id>/',views.detail,name ='detail'),
