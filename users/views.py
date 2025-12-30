92940fd1541b2682219b563f248b39c5690d94ee
Geethika <april.appy24@gmail.com>
2025-12-31 01:57:32 +0530

    Adding Additional Fields

--- users/views.py
+++ users/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import RegisterForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save() 
             username = form.cleaned_data.get('username')
             messages.success(request,f'Welcome {username}, your account is created.')
             return redirect('display:index')
     else: #Important indentation*
-            form = UserCreationForm()
+            form = RegisterForm()
 
     return render(request,'user/register.html',{'form' : form})
